8b17d65b2b7005e0f662da88050993f9d5c61871
Parminder singh <66919366+parminder-tech@users.noreply.github.com>
2022-09-20 13:50:35 +0530

    init project files

---
+++ src/App.vue
@@ -0,0 +1,151 @@
+<template>
+  <div id="app">
+    <section>
+      <div class="heading">
+        We make creative media that <span>adds value</span>
+      </div>
+
+      <div class="card-container">
+        <div class="col" v-for="item in items" :key="item.id">
+          <Card :cardItem="item" />
+        </div>
+      </div>
+      <div class="center">
+        <span role="button" class="btn">get started</span>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+import Card from "./components/Card.vue";
+
+export default {
+  name: "App",
+  components: {
+    Card,
+  },
+  data() {
+    return {
+      items: [
+        {
+          id: 1,
+          img: require(`@/assets/1.png`),
+          title: "Photography",
+          list: ["Drone Photography", "Interiors", "Exteriors / Architectural"],
+          desc: "Every photograph we produce finds the beauty in your property while serving as a profitable asset.",
+        },
+        {
+          id: 2,
+          img: require(`@/assets/2.png`),
+          title: "Virtual Staging",
+          list: [],
+          desc: "Our staging will help you sell the potential of your space.We can work off of our own photography or photos you provide.",
+        },
+        {
+          id: 3,
+          img: require(`@/assets/image9.png`),
+          title: "Renderings",
+          list: ["Interiors", "Exteriors"],
+          desc: "Renderings are the gold standard in pre-construction marketing.",
+        },
+        {
+          id: 4,
+          img: require(`@/assets/image10.png`),
+          title: "Interactive Tours",
+          list: ["Matterport", "Video Walkthroughs"],
+          desc: "Give your audience the ability to explore your property without requiring in-person travel.",
+        },
+        {
+          id: 5,
+          img: require(`@/assets/image11.png`),
+          title: "Floor Plans",
+          list: ["On-site Measure", "2D Floor Plans", "3D Floor Plans"],
+          desc: "We produce laser-precise floor plans faster than any other service.",
+        },
+        {
+          id: 6,
+          img: require(`@/assets/image12.png`),
+          title: "Video",
+          list: [
+            "Drone Cinematography",
+            "Interiors",
+            "Exteriors / Architectural",
+          ],
+          desc: "Video content has higher engagement and will help your property stand out online.",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+body {
+  background: #e5e5e5;
+  box-sizing: border-box;
+  line-height: 1.5;
+}
+#app {
+  font-family: "Poppins", sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  max-width: 1400px;
+  width: 100%;
+  margin: auto;
+  padding: 1rem;
+  section {
+    margin-top: 3rem;
+    .heading {
+      color: #1b264f;
+      font-weight: 700;
+      font-size: 56px;
+      line-height: 1.5;
+      text-align: center;
+      max-width: 750px;
+      width: 100%;
+      margin: auto;
+      margin-bottom: 3rem;
+      span {
+        color: #506bca;
+      }
+    }
+    .card-container {
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: space-between;
+      .col {
+        width: 32.5%;
+        margin-bottom: 1rem;
+        height: 100%;
+        @media (max-width: 1200px) {
+          width: 49%;
+        }
+        @media (max-width: 768px) {
+          width: 100%;
+        }
+      }
+    }
+  }
+}
+.center {
+  text-align: center;
+  margin: 2rem auto;
+}
+.btn {
+  padding: 20px 58px;
+  gap: 10px;
+  width: 315px;
+  height: 60px;
+  background: #1b264f;
+  border-radius: 12px;
+  text-align: center;
+  letter-spacing: 0.2em;
+  text-transform: uppercase;
+  font-weight: 700;
+  font-size: 17px;
+  line-height: 60px;
+  color: #ffffff;
+  cursor: pointer;
+}
+</style>
---
+++ src/components/Card.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="card">
+    <div class="img">
+      <img :src="cardItem.img" alt="" />
+    </div>
+    <div class="details">
+      <div class="title">{{cardItem.title}}</div>
+      <ul>
+        <li v-for="(item, index) in cardItem.list" :key="index">{{item}}</li>
+      </ul>
+      <p>{{cardItem.desc}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    props:{
+        cardItem:{}
+    }
+};
+</script>
+
+<style lang="scss">
+.card {
+  display: flex !important;
+  overflow: hidden;
+  background: #ffffff;
+  border-radius: 8px;
+  img {
+    height: 100%;
+  }
+  .details {
+    padding: 1.5rem;
+    .title {
+      font-weight: 700;
+      font-size: 24px !important;
+      color: #353844;
+    }
+    ul {
+      list-style-type: none;
+      margin-bottom: 0;
+      margin-bottom: 0.5rem;
+      padding: 0;
+    }
+    li {
+      font-family: "Nunito Sans", sans-serif !important;
+      font-weight: 700;
+      font-size: 15px;
+      line-height: 26px;
+      color: #353844;
+      position: relative;
+      padding-left: 1rem;
+      &::before{
+        position: absolute;
+        content: '';
+        height: 6px;
+        width: 6px;
+        background: #D2AD81;
+        left: 0;
+        top: 50%;
+        border-radius: 50%;
+        transform: translateY(-50%);
+      }
+    }
+    p {
+      font-family: "Nunito Sans", sans-serif !important;
+      font-style: normal;
+      font-weight: 600;
+      font-size: 15px;
+      line-height: 24px;
+      color: #353844;
+    }
+  }
+}
+</style>
